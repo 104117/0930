951b96a2902e467033537ead0ad8c7535603d7ba
104117 <70998436+104117@users.noreply.github.com>
2020-10-07 14:54:46 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,13 +21,26 @@ input.onGesture(Gesture.ScreenUp, function () {
     radio.sendString("S")
     basic.showIcon(IconNames.No)
 })
+function 前進 () {
+    while (bitbot.sonar(BBPingUnit.Centimeters) > 10) {
+        bitbot.go(BBDirection.Forward, 100)
+        if (bitbot.sonar(BBPingUnit.Centimeters) <= 10) {
+            bitbot.stop(BBStopMode.Brake)
+            break;
+        }
+    }
+    bitbot.stop(BBStopMode.Brake)
+}
 input.onGesture(Gesture.LogoDown, function () {
     radio.sendString("F")
     basic.showArrow(ArrowNames.North)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("D")
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "F") {
-        bitbot.go(BBDirection.Forward, 100)
+        前進()
         bitbot.ledClear()
         bitbot.setLedColor(0xFFC000)
     } else if (receivedString == "B") {
@@ -45,6 +58,28 @@ radio.onReceivedString(function (receivedString) {
     } else if (receivedString == "S") {
         bitbot.stop(BBStopMode.Brake)
         bitbot.ledClear()
+    } else if (receivedString == "D") {
+        for (let index = 0; index < 200000000000000; index++) {
+            bitbot.buzz(true)
+            bitbot.buzz(false)
+            basic.pause(1)
+            bitbot.buzz(true)
+            basic.pause(1)
+            bitbot.buzz(false)
+            basic.pause(1)
+            bitbot.buzz(true)
+            basic.pause(1)
+            bitbot.buzz(false)
+            basic.pause(1)
+            bitbot.buzz(true)
+            basic.pause(1)
+            bitbot.buzz(false)
+            basic.pause(1)
+            bitbot.buzz(true)
+            bitbot.buzz(false)
+            basic.pause(50)
+        }
+        bitbot.buzz(false)
     } else {
         bitbot.stop(BBStopMode.Brake)
         bitbot.ledClear()
